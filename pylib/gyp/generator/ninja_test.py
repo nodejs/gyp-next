bcb26581ee21acdca6eea730c9cb4c6aaba87990
Chengzhong Wu <chengzhong.wcz@alibaba-inc.com>
2024-03-08 17:57:53 +0800

    fixup!

--- pylib/gyp/generator/ninja_test.py
+++ pylib/gyp/generator/ninja_test.py
@@ -6,7 +6,7 @@
 
 """ Unit tests for the ninja.py file. """
 
-import os
+from pathlib import Path
 import sys
 import unittest
 
@@ -52,14 +52,15 @@ class TestPrefixesAndSuffixes(unittest.TestCase):
         )
 
     def test_GenerateCompileDBWithNinja(self):
-        this_dir = os.path.abspath(os.path.dirname(__file__))
-        build_dir = os.path.normpath(os.path.join(this_dir, "../../../data/ninja"))
+        build_dir = (
+            Path(__file__).resolve().parent.parent.parent.parent / "data" / "ninja"
+        )
         compile_db = ninja.GenerateCompileDBWithNinja(build_dir)
-        self.assertEqual(len(compile_db), 1)
-        self.assertEqual(compile_db[0]["directory"], build_dir)
-        self.assertEqual(compile_db[0]["command"], "cc my.in my.out")
-        self.assertEqual(compile_db[0]["file"], "my.in")
-        self.assertEqual(compile_db[0]["output"], "my.out")
+        assert len(compile_db) == 1
+        assert compile_db[0]["directory"] == str(build_dir)
+        assert compile_db[0]["command"] == "cc my.in my.out"
+        assert compile_db[0]["file"] == "my.in"
+        assert compile_db[0]["output"] == "my.out"
 
 
 if __name__ == "__main__":
