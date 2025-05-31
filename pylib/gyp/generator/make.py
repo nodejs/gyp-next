59b59035f4ae63419343ffdafe0f0ff511ada17d
Jack <jack4zhang@gmail.com>
2025-05-31 22:27:00 +0800

    fix: Ensure Consistent Order of build_files in WriteAutoRegenerationRule (#293)

--- pylib/gyp/generator/make.py
+++ pylib/gyp/generator/make.py
@@ -2381,7 +2381,7 @@ def WriteAutoRegenerationRule(params, root_makefile, makefile_name, build_files)
         % {
             "makefile_name": makefile_name,
             "deps": replace_sep(
-                " ".join(SourceifyAndQuoteSpaces(bf) for bf in build_files)
+                " ".join(sorted(SourceifyAndQuoteSpaces(bf) for bf in build_files))
             ),
             "cmd": replace_sep(gyp.common.EncodePOSIXShellList(
                 [gyp_binary, "-fmake"] + gyp.RegenerateFlags(options) + build_files_args
